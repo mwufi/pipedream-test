84013a97d5e327be328e757d539a4421caa1ce53
Zen T <ztang230@gmail.com>
2025-06-20 20:13:18 -0500

    k

--- apps/futuristic-mail/lib/instant_clientside_db.ts
+++ apps/futuristic-mail/lib/instant_clientside_db.ts
@@ -4,9 +4,7 @@ import { init } from "@instantdb/react";
 const db = init({
     appId: "b6c62e9f-d6c9-4722-9242-6263312dd44d", 
     schema: schema,
-    devtool: {
-        position: 'bottom-left',
-    }
+    devtool: false
 });
 
 export default db;
